fe5869847719fb16d1f0c066327626d0b519e913
Andreas Lüdemann <andreaslydemann@gmail.com>
2021-02-19 20:50:44 +0100

    Initial Commit

---
+++ LudemannAcademy/LudemannAcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LudemannAcademyApp.swift
+//  LudemannAcademy
+//
+//  Created by Andreas on 19/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct LudemannAcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
