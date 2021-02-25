d84c79a32d62dc09cd0c91bc2dce8b59dde19dca
Andreas Lüdemann <andreaslydemann@gmail.com>
2021-02-25 12:24:53 +0100

    Fix jump when switching text field

--- AcademyUI/AcademyUI/Source/KeyboardActions.swift
+++ AcademyUI/AcademyUI/Source/KeyboardActions.swift
@@ -47,14 +47,7 @@ extension UIApplication {
         let tapGesture = UITapGestureRecognizer(target: window, action: #selector(UIView.endEditing))
         tapGesture.requiresExclusiveTouchType = false
         tapGesture.cancelsTouchesInView = false
-        tapGesture.delegate = self
         window.addGestureRecognizer(tapGesture)
-    }
-}
-
-extension UIApplication: UIGestureRecognizerDelegate {
-    public func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        return true
     }
 }
 
