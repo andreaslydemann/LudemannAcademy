06a44b8ba5e63307c563939370f2bf1e39df2c6e
Andreas Lüdemann <andreaslydemann@gmail.com>
2021-03-03 07:42:04 +0100

    Clean up spacing

--- AcademyUI/AcademyUI/Source/Spacing.swift
+++ AcademyUI/AcademyUI/Source/Spacing.swift
@@ -3,24 +3,34 @@ import SwiftUI
 public enum Spacing: CGFloat, CaseIterable, Equatable {
     /// Pixel size: 0
     case zero = 0
+    
     /// Pixel size: 4
     case tiny = 4
+    
     /// Pixel size: 8
     case xSmall = 8
+    
     /// Pixel size: 16
     case small = 16
+    
     /// Pixel size: 24
     case medium = 24
+    
     /// Pixel size: 32
     case large = 32
+    
     /// Pixel size: 40
     case xLarge = 40
+    
     /// Pixel size: 48
     case xxLarge = 48
+    
     /// Pixel size: 64
     case xxxLarge = 64
+    
     /// Pixel size: 80
     case xxxxLarge = 80
+    
     /// Pixel size: 140
     case xxxxxLarge = 140
 }
